5ea55be3a2c454bb843ba698384809fda8bbdc73
Trml <tremouille.sylvain@gmail.com>
2019-03-05 13:53:40 +0100

    Adding PHPUnit via composer + first exemple test

---
+++ UnitTests/Test/FirstTest.php
@@ -0,0 +1,14 @@
+<?php
+namespace UnitTests\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class FirstTest extends TestCase
+{
+    public function testTrueAssetsToTrue()
+    {
+        $condition = true;
+        $this->assertTrue($condition);
+    }
+}
+?>
